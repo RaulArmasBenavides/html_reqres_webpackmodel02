582c7e8b3f7417f79650ff8ae9a1e98e77bb07c7
Raúl Armas <raularmasbx@gmail.com>
2021-11-28 14:54:51 -0500

    modifications

--- devExpressSample/js/index.js
+++ devExpressSample/js/index.js
@@ -1,29 +1,3 @@
-import { dxChart } from "https://cdn3.devexpress.com/jslib/21.2.3/js/dx.all.js";
-
-const populationData = [{
-  arg: 1960,
-  val: 3032019978,
-}, {
-  arg: 1970,
-  val: 3683676306,
-}, {
-  arg: 1980,
-  val: 4434021975,
-}, {
-  arg: 1990,
-  val: 5281340078,
-}, {
-  arg: 2000,
-  val: 6115108363,
-}, {
-  arg: 2010,
-  val: 6922947261,
-}, {
-  arg: 2020,
-  val: 7795000000,
-}];
-
-
 let options = {
     dataSource: populationData,
     legend: {
@@ -43,4 +17,5 @@ let options = {
     title: 'World Population by Decade',
   };
 
-  new DevExpress.viz.dxChart(document.getElementById('chart'), options);
+
+  new dxChart(document.getElementById('chart'), options);
