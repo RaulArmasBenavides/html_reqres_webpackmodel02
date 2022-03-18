c23367676cf0708080c1e27255c59edf99c547ea
Raúl Armas <raularmasbx@gmail.com>
2022-03-18 18:47:54 -0500

    update

--- localstoragemodels/app2ls.js
+++
@@ -1,6 +0,0 @@
-var formulario = document.querySelector("#formfilms");
-formulario.addEventListener('submit',function()
-{
-    var peli = document.querySelector("#addfilms");
-    localStorage.setItem(peli,peli);
-});
