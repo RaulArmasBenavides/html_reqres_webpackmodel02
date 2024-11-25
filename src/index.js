cf973f5b1ecd3f0464c0ffb0b95167bb5c32ca6c
Raul Armas <raul.armas@intelica.com>
2024-11-25 16:42:11 -0500

    lodash samples

--- src/index.js
+++ src/index.js
@@ -1,38 +1,67 @@
 // import { heroesCiclo, heroeIfAwait } from './js/await';
-import {getInfo,CreateHero} from './js/promesas';
+import { getInfo, CreateHero } from "./js/promesas";
 
+import * as CRUD from "./js/promesas";
+import { shuffle, debounce, uniqBy } from "lodash";
+// getInfo();
 
-import * as CRUD from './js/promesas';
+// let hero = {
+//    name: "Fernando",
+//    job: "Carpintero",
+// };
 
+// CRUD.CreateHero(hero).then(console.log);
+// hero = {
+//    name: "Raúl",
+//    job: "Ingeniero",
+// };
 
-getInfo();
+// CRUD.MostrarHeroe(hero);
 
+// CRUD.CreateHero(hero).then(console.log);
+// hero = {
+//    name: "Miguel",
+//    job: "Abogado",
+// };
+// CRUD.MostrarHeroe(hero);
+// CRUD.CreateHeroAsync(hero).then(console.log);
 
-let hero = {
-    name: 'Fernando',
-    job: 'Carpintero'
+const array = [1, 2, 3, 4];
+const shuffledArray = shuffle(array);
+console.log(shuffledArray);
+
+const debouncedFunc = debounce(
+   () => console.log("Ejecutado después de un retraso"),
+   300
+);
+debouncedFunc();
+
+const original = {
+   id: 1,
+   details: {
+      name: "John",
+      age: 30,
+   },
 };
 
-CRUD.CreateHero(hero).then( console.log );
- hero = {
-    name: 'Raúl',
-    job: 'Ingeniero'
-};
+// 1. Copia profunda con _.cloneDeep
+const deepCopy = _.cloneDeep(original);
+deepCopy.details.name = "Jane";
 
-CRUD.MostrarHeroe(hero);
+// 2. Copia superficial con spread
+const shallowCopy = { ...original };
+shallowCopy.details.age = 25;
 
-CRUD.CreateHero(hero).then( console.log );
- hero = {
-    name: 'Miguel',
-    job: 'Abogado'
-};
-CRUD.MostrarHeroe(hero);
-CRUD.CreateHeroAsync(hero).then( console.log );
+console.log("Original:", original);
+// Salida: { id: 1, details: { name: 'John', age: 25 } } <- Cambia porque la referencia es compartida con shallowCopy
 
+console.log("Deep Copy:", deepCopy);
+// Salida: { id: 1, details: { name: 'Jane', age: 30 } } <- No afecta al original
 
-
+console.log("Shallow Copy:", shallowCopy);
+// Salida: { id: 1, details: { name: 'John', age: 25 } }
 
 
+original.details.age = 35;
 
-
-
+console.log("Shallow Copy:", shallowCopy);
